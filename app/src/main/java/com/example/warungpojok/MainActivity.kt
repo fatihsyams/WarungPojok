35c16a8aebb8058719e24f8cb88c050b872cf90e
fatih <fatihsyams21@gmail.com>
2020-08-05 10:11:55 +0700

    add base app

--- app/src/main/java/com/example/warungpojok/MainActivity.kt
+++ app/src/main/java/com/example/warungpojok/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.example.warungpojok
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setIntentFragment()
+        loadFragment(PesananFragment())
+
 
     }
-    private fun setIntentFragment() {
-        tvPesananMain.setOnClickListener {
-            applicationContext?.let {
-                val intent = Intent(it, PesananFragment::class.java)
-                it.startActivity(intent)
-            }
-        }
+
+    private fun loadFragment(fragment : Fragment) {
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
+
+//    private fun setIntentFragment() {
+//        tvPesananMain.setOnClickListener {
+//            applicationContext?.let {
+//                val intent = Intent(it, PesananFragment::class.java)
+//                it.startActivity(intent)
+//            }
+//        }
+//    }
 }
